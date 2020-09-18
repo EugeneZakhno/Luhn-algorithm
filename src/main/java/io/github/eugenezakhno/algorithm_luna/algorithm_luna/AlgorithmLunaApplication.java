5a8a0d95860247a4911ac9f4f79f0a2ffaff539c
EugeneZakhno <zohan1234567@gmail.com>
2020-09-18 22:56:37 +0300

    init

---
+++ src/main/java/io/github/eugenezakhno/algorithm_luna/algorithm_luna/AlgorithmLunaApplication.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.algorithm_luna.algorithm_luna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmLunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmLunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/eugenezakhno/algorithm_luna/algorithm_luna/AlgorithmLunaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.algorithm_luna.algorithm_luna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmLunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
